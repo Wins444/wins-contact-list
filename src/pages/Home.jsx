5fd74fdbd30f1511cd3e11d5a2374a5305d82134
Wins444 <winsking04yt@gmail.com>
2025-04-12 09:54:19 +0000

    empezando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
+import React, { useEffect, useState } from "react";
+
+
 
 import { Link, useNavigate } from "react-router-dom";
 
@@ -7,6 +11,38 @@ export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
 
+
+  
+	useEffect(() => {
+
+		const fetchContacts = async () => {
+			try {
+				const response = await fetch('https://playground.4geeks.com/contact/agendas/Roberto');
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				const data = await response.json();
+				
+				dispatch({
+					type: "setContacts",
+					payload: data.contacts
+				})
+					
+				console.log("datos guardados")
+				console.log(data.contacts)
+				
+
+			} catch (error) {
+				console.error("Hubo un problema con la solicitud:", error);
+			}
+
+		};
+
+		fetchContacts();
+	}, []);
+
+
+	// con un map renderizar esos contactos
 	return (
 		<div className="text-center mt-5">
 			<Link to="/CreateUser">
--- src/store.js
+++ src/store.js
@@ -1,32 +1,43 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+export const initialStore = () => {
+  return {
+
+    contacts: [],
+    userRole: ""
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
 
-      const { id,  color } = action.payload
+  switch (action.type) {
+
+    case 'setContacts':
 
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        contacts: action.payload
       };
+
+    case "addContact":
+      return {
+        ...store,
+        contacts: [...store.contacts, action.payload]
+      };
+
+
+    case "editContact":
+      return {
+        ...store,
+        contacts: [...store.contacts, action.payload]
+      };
+
+    case "deleteContact":
+      return {
+        ...store,
+        contacts: store.contacts.filter(contact => contact.id !== action.payload)
+      };
+
+
     default:
-      throw Error('Unknown action.');
-  }    
+      return store;
+  }
 }
